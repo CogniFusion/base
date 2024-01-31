565a646643955fc61da9325464171f8b471cccd5
dithiane <elenamena.colorado@gmail.com>
2024-01-31 11:32:51 -0700

    [CG-66] Connecting Next.js app to MongoDB using Prisma, removed mongoose files

--- lib/actions/user.actions.tsx
+++ lib/actions/user.actions.tsx
@@ -1,8 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function fetchUser(userId: string) {
+/**
+ * Asynchronously fetches a user using the provided userId.
+ *
+ * @param {string} userId - The unique identifier of the user to fetch.
+ * @return {Promise<User>} The user object that is fetched.
+ */
+export async function fetchUser(userId: string): Promise<User> {
     try {
         const user = await prisma.user.findFirst({
             where: {
--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
     experimental: {
         serverActions: true, //enables experimental server actions, which allows for server-side logic to be performed during the server rendering phase of Next.js.
-        serverComponentsExternalPackages: ["mongoose"], //allows the usage of the "mongoose" package within server components.
     }
 
 }
