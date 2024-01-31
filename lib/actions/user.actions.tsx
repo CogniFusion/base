ed9fa2368987f907b805f528ed66bd23d59e5caf
dithiane <elenamena.colorado@gmail.com>
2024-01-31 11:26:37 -0700

    [CG-66] Connecting NExt.js app to MongoDB Atlas using Prismae

--- lib/actions/user.actions.tsx
+++ lib/actions/user.actions.tsx
@@ -1,23 +1,16 @@
-"use server"
+import { PrismaClient } from '@prisma/client';
 
-import User from "../models/user.model"
-import { connectToDB } from "../mongoose"
+const prisma = new PrismaClient();
 
-/**
- * Fetches a user from the database by their ID.
- *
- * @param {String} userId - The ID of the user to fetch.
- * @return {Promise<User>} A promise that resolves to the fetched user.
- */
-export async function fetchUser(userId: String): Promise<typeof User> {
+export async function fetchUser(userId: string) {
     try {
-        connectToDB() // Establish database connection
-        const user = await User.findOne({ id: userId }); // Find user by ID
-        if (!user) {
-            throw new Error(`User not found`);
-        }
+        const user = await prisma.user.findFirst({
+            where: {
+                id: userId,
+            },
+        });
         return user;
     } catch (error: any) {
-        throw new Error(`Failed to fetch user: ${error}`)
+        throw new Error(`Failed to fetch user: ${error}`);
     }
 }
